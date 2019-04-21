c63a4d8e6dfa1f6311b597be453890eb407d53c1
Christopher Stanley <cbstanley12@gmail.com>
2019-04-21 17:34:27 -0600

    Set up and define functions for the game

--- app.js
+++ app.js
@@ -15,3 +15,31 @@ const game = document.querySelector('#game'),
 // Assign the min and max values to UI elements
 minNum.textContent = min;
 maxNum.textContent = max;
+
+// Determine the final state of the game
+function gameOver(won, msg){
+    let color;
+    won === true ? color = 'green' : color = 'red';
+
+    // Disable the number input
+    guessInput.disabled = true;
+    // Change the border color of the input
+    guessInput.getElementsByClassName.borderColor = color;
+    // Display message to the user
+    setMessage(msg, color);
+
+    // Prompt the user to play again
+    guessBtn.value = 'Play Again';
+    guessBtn.className += 'play-again';
+}
+
+// Generate a random number as the winning number
+function getRandomNum(min, max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+// Display a message to the user based on the game's outcome
+function setMessage(msg, color){
+    message.style.color = color;
+    message.textContent = msg;
+}
